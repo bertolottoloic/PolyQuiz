640fa06251665cdea79af5c0789e1ba38d8a659b
ClementPoueyto <poueyto.clement@gmail.com>
2020-03-10 11:50:00 +0100

    merge

--- PolyQuiz-zeCommit/src/app/app.routing.module.ts
+++ PolyQuiz-zeCommit/src/app/app.routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './accueil/accueil.component';
 
+
 const routes:Routes=[
   { path: '', component : AccueilComponent },
 ]
