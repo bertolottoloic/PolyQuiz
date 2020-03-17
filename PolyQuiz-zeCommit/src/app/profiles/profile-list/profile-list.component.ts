45475e46dba39b4acf7f1c1cc944e932c0b83bc3
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-03-17 10:15:57 +0100

    profile-list

--- PolyQuiz-zeCommit/src/app/profiles/profile-list/profile-list.component.ts
+++ PolyQuiz-zeCommit/src/app/profiles/profile-list/profile-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProfileService } from '../../services/profile.service';
 import { Profile } from '../../models/profile.model';
 import { PROFILE_LIST } from 'src/app/mocks/profiles-list.mock'
+import { Handicap } from 'src/app/models/profile.model'
 
 
 @Component({
@@ -11,10 +12,27 @@ import { PROFILE_LIST } from 'src/app/mocks/profiles-list.mock'
 })
 export class ProfileListComponent implements OnInit {
 
-  public profileList: Profile[] = [];
+  public profileList: Profile[] = [{
+    name: 'Dacosta',
+    firstName: 'Pedro',
+    id:'1',
+    trouble: Handicap.Memoire
+},
+{
+    name: 'Meulle',
+    firstName: 'Nathan',
+    id:'2',
+    trouble: Handicap.Moteur
+},
+{
+    name: 'Delm',
+    firstName: 'Vinze',
+    id:'3',
+    trouble: Handicap.Vue
+}];
 
   constructor(public profileService: ProfileService) {
-    this.profileService.profiles$.subscribe((profile) => this.profileList = profile);
+    this.profileService.profiles$.subscribe((profiles: Profile[]) => {this.profileList = profiles;});
   }
 
   ngOnInit() {
