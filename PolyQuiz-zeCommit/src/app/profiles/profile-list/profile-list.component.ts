1076c83c0cda3c93463c2bd1f8b6ab675d428823
ClementPoueyto <poueyto.clement@gmail.com>
2020-03-17 10:27:26 +0100

    mock link

--- PolyQuiz-zeCommit/src/app/profiles/profile-list/profile-list.component.ts
+++ PolyQuiz-zeCommit/src/app/profiles/profile-list/profile-list.component.ts
@@ -12,24 +12,7 @@ import { Handicap } from 'src/app/models/profile.model'
 })
 export class ProfileListComponent implements OnInit {
 
-  public profileList: Profile[] = [{
-    name: 'Dacosta',
-    firstName: 'Pedro',
-    id:'1',
-    trouble: Handicap.Memoire
-},
-{
-    name: 'Meulle',
-    firstName: 'Nathan',
-    id:'2',
-    trouble: Handicap.Moteur
-},
-{
-    name: 'Delm',
-    firstName: 'Vinze',
-    id:'3',
-    trouble: Handicap.Vue
-}];
+  public profileList: Profile[] = [];
 
   constructor(public profileService: ProfileService) {
     this.profileService.profiles$.subscribe((profiles: Profile[]) => {this.profileList = profiles;});
