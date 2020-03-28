ce6714c7ea6517e7edbb0a1034437f7e2f4e11f4
ClementPoueyto <poueyto.clement@gmail.com>
2020-03-28 17:27:00 +0100

    modif css

--- PolyQuiz-zeCommit/src/app/quizPage/quiz-page-memory/quiz-page-memory.component.ts
+++ PolyQuiz-zeCommit/src/app/quizPage/quiz-page-memory/quiz-page-memory.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Quiz} from '../../models/quiz.models';
 import{ QuizListService} from '../../services/quizList.service';
+import{ ProfileService} from '../../services/profile.service';
+
 import { ActivatedRoute } from '@angular/router';
 import { Question } from 'src/app/models/question.models';
 import { StatMemory } from 'src/app/models/stat.models';
 import { stat } from 'fs';
 import { Answer } from 'src/app/models/answer.models';
+import { Profile } from 'src/app/models/profile.models';
 
 @Component({
   selector: 'app-quiz-page-memory',
@@ -14,6 +17,7 @@ import { Answer } from 'src/app/models/answer.models';
 })
 export class QuizPageMemoryComponent implements OnInit {
 
+  public profile: Profile;
   public quiz: Quiz;
   public questionList: Question[];
   public question: Question;
@@ -24,22 +28,40 @@ export class QuizPageMemoryComponent implements OnInit {
   private timer: number;
 
 
-  constructor(public quizService: QuizListService,private route: ActivatedRoute) {
-    let id:number;
-    this.route.paramMap.subscribe(params => {
-      id=Number(params.get('idQuiz'))
-      this.quizService.quizzes$.subscribe((quizzes) => {
-        let quiz= quizzes.filter((quiz)=>quiz.id==id)[0]
-        if(quiz){
-          this.quiz=quiz
-          this.stats=new StatMemory(quiz); //creation objet stat
-          this.questionList=quiz.questions
-          this.question=quiz.questions[this.index];
-        }
-      })
-    })
+  constructor(public profileService:ProfileService,public quizService: QuizListService,private route: ActivatedRoute) {
+    this.loadQuiz();
+    this.loadProfile();
     this.timer=Date.now(); //debut chrono
 
+}
+
+loadQuiz(){
+  let id:number;
+  this.route.paramMap.subscribe(params => {
+    id=Number(params.get('idQuiz'))
+    this.quizService.quizzes$.subscribe((quizzes) => {
+      let quiz= quizzes.filter((quiz)=>quiz.id==id)[0]
+      if(quiz){
+        this.quiz=quiz
+        this.stats=new StatMemory(quiz); //creation objet stat
+        this.questionList=quiz.questions
+        this.question=quiz.questions[this.index];
+      }
+    })
+  })
+}
+
+loadProfile(){
+  let id:number;
+  this.route.paramMap.subscribe(params => {
+    id=Number(params.get('idProfile'))
+    this.profileService.profiles$.subscribe((profiles) => {
+      let profil= profiles.filter((prf)=>prf.id==id)[0]
+      if(profil){
+        this.profile=profil;
+      }
+    })
+  })
 }
 
   ngOnInit() {
