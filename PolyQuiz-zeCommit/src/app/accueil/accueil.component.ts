b11af0804eccb2b10ff6d23ae35ff0bcd319d278
bertolottoloic <loic.bertolotto@etu.unice.fr>
2020-03-31 11:00:52 +0200

    man

--- PolyQuiz-zeCommit/src/app/accueil/accueil.component.ts
+++ PolyQuiz-zeCommit/src/app/accueil/accueil.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import { QuizListService } from '../services/quizList.service';
 import { Handicap } from '../models/handicap.models';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-accueil',
@@ -10,7 +11,7 @@ import { Handicap } from '../models/handicap.models';
 })
 export class AccueilComponent implements OnInit {
 
-  constructor(private router: Router, public quizListService:QuizListService) {
+  constructor(private router: Router, public quizListService:QuizListService, public profileService:ProfileService) {
     this.router = router;
   }
 
@@ -20,10 +21,13 @@ export class AccueilComponent implements OnInit {
   setCurrentTrouble(trouble:string){
     if(trouble==="memoire"){
       this.quizListService.currentTrouble = Handicap.Memoire;
+      this.profileService.currentTrouble = Handicap.Memoire;
     } else if(trouble==="vue"){
       this.quizListService.currentTrouble = Handicap.Vue;
+      this.profileService.currentTrouble = Handicap.Vue;
     } else if(trouble==="moteur"){
       this.quizListService.currentTrouble = Handicap.Moteur;
+      this.profileService.currentTrouble = Handicap.Moteur;
     }
     console.log(this.quizListService.currentTrouble);
   }
