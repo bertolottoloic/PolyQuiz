4728cdadaa2770d7f858be79ef3c83fdd5a66685
ClementPoueyto <poueyto.clement@gmail.com>
2020-04-07 16:30:54 +0200

    debug quiz creation

--- PolyQuiz-zeCommit/src/app/manage/creation-page/quiz-create-question-page/quiz-create-question-page.component.ts
+++ PolyQuiz-zeCommit/src/app/manage/creation-page/quiz-create-question-page/quiz-create-question-page.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { QuizListService } from 'src/app/services/quizList.service';
 import { Answer } from 'src/app/models/answer.models';
 import { Question } from 'src/app/models/question.models';
-import { ActivatedRoute } from '@angular/router';
-import { MatDialog } from '@angular/material/dialog';
-import { QuestionComponent } from 'src/app/quizPage/question/question-memory/question-memory.component';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-quiz-create-question-page',
@@ -20,7 +19,7 @@ export class QuizCreateQuestionPageComponent implements OnInit {
   question:Question;
   
   
-  constructor(public formBuilder:FormBuilder, public quizListService:QuizListService,public route:ActivatedRoute) { 
+  constructor(public formBuilder:FormBuilder, public quizListService:QuizListService,public route:ActivatedRoute,public router:Router) { 
     this.route.paramMap.subscribe(params => {
       if(params!=null){
         this.quizId = Number(params.get('quizId'))}
@@ -40,13 +39,23 @@ export class QuizCreateQuestionPageComponent implements OnInit {
       this.initializeQuestionForm();
     }
   }
+  
+  controlRightAnswer():boolean{
+    let nbTrue=0;
+    this.answers.value.forEach(element => {
+      if(element.isCorrect==true){
+        nbTrue++;      
+      }
+    });
+    return nbTrue>0?true:false;
+  }
 
 
   get formData() { return <FormArray>this.questionForm.get('answers'); }
 
   createAnswer(): FormGroup {
     return this.formBuilder.group({
-      text: [''],
+      text: ['',Validators.required],
       isCorrect: [false],
     });
   }
@@ -61,10 +70,11 @@ export class QuizCreateQuestionPageComponent implements OnInit {
   }
 
   addQuestion() {
-    if(this.questionForm.valid) {
+    if(this.questionForm.valid&&this.controlRightAnswer()) {
       const question = this.questionForm.getRawValue() as Question;
       this.quizListService.addQuestion(this.quizId, question);
       this.initializeQuestionForm();
+      this.router.navigate(["../"],{ relativeTo: this.route })
     }
   }
 
@@ -73,12 +83,14 @@ export class QuizCreateQuestionPageComponent implements OnInit {
       const question = this.questionForm.getRawValue() as Question;
       this.quizListService.editQuestion(this.quizId, question);
       this.initializeQuestionForm();
+      this.router.navigate(["../"],{ relativeTo: this.route })
+
     }
   }
 
   initializeQuestionForm() {
     this.questionForm = this.formBuilder.group({
-      text:[''],
+      text:['',Validators.required],
       image:[''],
       answers: this.formBuilder.array([this.createAnswer()]),
     });
@@ -97,7 +109,6 @@ export class QuizCreateQuestionPageComponent implements OnInit {
 
   fillAnswers(){
     this.answers = this.questionForm.get('answers') as FormArray;
-    console.log(this.answers)
     for(let i = 1; i<4 ; i++)  {
         this.answers.push(this.fillAnswer(this.question.answers[i]));
       }
@@ -110,6 +121,14 @@ export class QuizCreateQuestionPageComponent implements OnInit {
     });
   }
 
+  invalidAnswer(index)
+  {
+  	return (this.answers.at(index).value.text=='');
+  }
+  invalidQuestion()
+  {
+  	return (this.questionForm.controls.text.errors!=null);
+  }
  
 
 }
