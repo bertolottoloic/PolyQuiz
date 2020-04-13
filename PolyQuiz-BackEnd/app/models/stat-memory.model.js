ad68e5301187c3b8fbf9f4bbf7eec726f0b7dbf5
ClementPoueyto <poueyto.clement@gmail.com>
2020-04-13 14:52:21 +0200

    css

--- PolyQuiz-BackEnd/app/models/stat-memory.model.js
+++ PolyQuiz-BackEnd/app/models/stat-memory.model.js
@@ -7,7 +7,6 @@ module.exports = new BaseModel('stat-memory', {
   score: Joi.number().required(),
   time: Joi.number().required(),
   trial: Joi.object().pattern(Joi.number(), Joi.number()),
-  score: Joi.array().required(),
   questionsDone: Joi.array().required(),
   nbRightAnswers: Joi.number().required(),
   nbWrongAnswers: Joi.number().required(),
