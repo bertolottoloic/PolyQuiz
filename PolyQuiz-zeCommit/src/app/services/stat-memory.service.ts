c29bb5d674683d2fc508ed35ac6fc9ff925e9f76
ClementPoueyto <poueyto.clement@gmail.com>
2020-04-13 14:01:03 +0200

    css

---
+++ PolyQuiz-zeCommit/src/app/services/stat-memory.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject,Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import { serverUrl, httpOptionsBase } from '../../configs/server.config';
+
+import { StatMemory } from '../models/stat.models';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ThemeService {
+
+  public stats: StatMemory[];
+
+  private URL : string = serverUrl+"/stat-memory";
+  public stats$: BehaviorSubject<StatMemory[]> = new BehaviorSubject(this.stats);
+  private httpOptions = httpOptionsBase;
+
+  constructor(private http:HttpClient) {
+    this.setStatsFromUrl();
+  }
+
+
+  setStatsFromUrl(){
+    this.http.get<StatMemory[]>(this.URL).subscribe((stat) =>{
+      this.stats = stat;
+      this.stats$.next(this.stats);
+    });
+  }
+
+}
+
