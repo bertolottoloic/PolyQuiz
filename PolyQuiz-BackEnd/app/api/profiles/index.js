91c200acb9c20e96e6084154990285e65ed8b1c7
bertolottoloic <loic.bertolotto@etu.unice.fr>
2020-04-26 16:17:47 +0200

    back

--- PolyQuiz-BackEnd/app/api/profiles/index.js
+++ PolyQuiz-BackEnd/app/api/profiles/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express')
 
-const { Profile } = require('../../models')
+const { Profile, Stat } = require('../../models')
 const StatRouter = require('./stats')
 
 const router = new Router()
@@ -39,9 +39,9 @@ router.post('/', (req, res) => {
 
 router.delete('/:profileId', (req, res) => {
   try {
-    // Stat.get().filter((stat)=> stat.profileId == req.params.profileId).forEach(stat => {
-    //   Stat.delete(stat.id)
-    // });
+    Stat.get().filter((stat)=> stat.profileId == req.params.profileId).forEach(stat => {
+      Stat.delete(stat.id)
+    });
     res.status(200).json(Profile.delete(req.params.profileId))
   } catch (err) {
     res.status(404).json(err)
